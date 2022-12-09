07f9384b63ebc81ff519695762763711009b36f9
Vipin Mishra <vipin@digiqt.com>
2022-12-09 16:36:13 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
-import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Flex, Loader } from "@mantine/core";
 import Layout from "./Components/Layout";
 
 // Pages
-const Home = lazy(() => import("./Pages/Home"));
-const AboutUs = lazy(() => import("./Pages/AboutUs"));
-const Infrastructure = lazy(() => import("./Pages/Infrastructure"));
-const ContactUs = lazy(() => import("./Pages/ContactUs"));
-const Staff = lazy(() => import("./Pages/Staff"));
-const StudentCouncil = lazy(() => import("./Pages/StudentCouncil"));
-const NewsEvents = lazy(() => import("./Pages/NewsEvents"));
-const Activities = lazy(() => import("./Pages/Activities"));
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import Infrastructure from "./Pages/Infrastructure";
+import ContactUs from "./Pages/ContactUs";
+import Staff from "./Pages/Staff";
+import StudentCouncil from "./Pages/StudentCouncil";
+import NewsEvents from "./Pages/NewsEvents";
+import Activities from "./Pages/Activities";
 
 const App = () => {
   return (
-    <Suspense
-      fallback={
-        <Flex py="md" align="center">
-          <Loader variant="dots" />
-        </Flex>
-      }
-    >
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/Infrastructure" element={<Infrastructure />} />
-          <Route path="/OrganizationalStructure" element={<Staff />} />
-          <Route path="/StudentCouncil" element={<StudentCouncil />} />
-          <Route path="/NewsEvents" element={<NewsEvents />} />
-          <Route path="/Activities" element={<Activities />} />
-          <Route path="/ContactUs" element={<ContactUs />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Infrastructure" element={<Infrastructure />} />
+        <Route path="/OrganizationalStructure" element={<Staff />} />
+        <Route path="/StudentCouncil" element={<StudentCouncil />} />
+        <Route path="/NewsEvents" element={<NewsEvents />} />
+        <Route path="/Activities" element={<Activities />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+      </Route>
+    </Routes>
   );
 };
 
