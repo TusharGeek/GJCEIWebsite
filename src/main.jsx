21263be79a518296d73f1d472e4325a92f99163a
Vipin Mishra <vipin@digiqt.com>
2022-12-10 11:23:08 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/600.css";
@@ -34,9 +34,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         },
       }}
     >
-      <BrowserRouter basename="/GJCEIWebsite">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </MantineProvider>
   </React.StrictMode>
 );
